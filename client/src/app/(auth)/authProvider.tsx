5344bea9275f6622fbbc4b3a52a2cbde9bd2de92
Kiet Dinh <kiet.dinh.t@gmail.com>
2026-01-19 18:24:42 -0600

    feat: finish the client-cognito part 2:57:00

---
+++ client/src/app/(auth)/authProvider.tsx
@@ -0,0 +1,136 @@
+import React, { useEffect } from "react";
+import { Amplify } from "aws-amplify";
+import {
+  Authenticator,
+  Heading,
+  Radio,
+  RadioGroupField,
+  useAuthenticator,
+  View,
+} from "@aws-amplify/ui-react";
+import "@aws-amplify/ui-react/styles.css";
+import { useRouter, usePathname } from "next/navigation";
+import formFields from "./formFields.json";
+
+// https://docs.amplify.aws/gen1/javascript/tools/libraries/configure-categories/
+Amplify.configure({
+  Auth: {
+    Cognito: {
+      userPoolId: process.env.NEXT_PUBLIC_AWS_COGNITO_USER_POOL_ID!,
+      userPoolClientId:
+        process.env.NEXT_PUBLIC_AWS_COGNITO_USER_POOL_CLIENT_ID!,
+    },
+  },
+});
+
+const Auth = ({ children }: { children: React.ReactNode }) => {
+  const { user } = useAuthenticator((context) => [context.user]); // this line requires <Authenticator.Provider> in providers.tsx to work
+
+  const router = useRouter();
+  const pathname = usePathname();
+
+  const isAuthPage = pathname.match(/^\/(signin|signup)$/);
+  const isDashboardPage =
+    pathname.startsWith("/manager") || pathname.startsWith("/tenants");
+
+  // After signing in, users still in sign in/sign up page, need
+  // to redirect authenticated users away
+  useEffect(() => {
+    if (user && isAuthPage) {
+      router.push("/");
+    }
+  }, [user, isAuthPage, router]);
+
+  // Allow access to public pages without authentication
+  // Without this, unauthenticated users will be stuck in auth pages
+  if (!isAuthPage && !isDashboardPage) {
+    return <>{children}</>;
+  }
+  return (
+    <div className="h-full">
+      <Authenticator
+        initialState={pathname.includes("signup") ? "signUp" : "signIn"} // without this, going to /signup keeps showing signIn form
+        components={components}
+        formFields={formFields}
+      >
+        {() => <>{children}</>}
+      </Authenticator>
+    </div>
+  );
+};
+
+const components = {
+  Header() {
+    return (
+      <View className="mt-4 mb-7">
+        <Heading level={3} className="!text-2xl !font-bold">
+          RENT
+          <span className="text-secondary-500 font-light hover:!text-primary-300">
+            IFUL
+          </span>
+        </Heading>
+        <p className="text-muted-foreground mt-2">
+          <span className="font-bold">Welcome!</span> Please sign in to continue
+        </p>
+      </View>
+    );
+  },
+  SignIn: {
+    Footer() {
+      const { toSignUp } = useAuthenticator();
+      return (
+        <View className="text-center mt-4">
+          <p className="text-muted-foreground">
+            Don&apos;t have an account?{" "}
+            <button
+              onClick={toSignUp}
+              className="text-primary hover:underline bg-transparent border-none p-0"
+            >
+              Sign up here
+            </button>
+          </p>
+        </View>
+      );
+    },
+  },
+  SignUp: {
+    FormFields() {
+      const { validationErrors } = useAuthenticator();
+
+      return (
+        <>
+          <Authenticator.SignUp.FormFields />
+          <RadioGroupField
+            legend="Role"
+            name="custom:role"
+            errorMessage={validationErrors?.["custom:role"]}
+            hasError={!!validationErrors?.["custom:role"]}
+            isRequired
+          >
+            <Radio value="tenant">Tenant</Radio>
+            <Radio value="manager">Manager</Radio>
+          </RadioGroupField>
+        </>
+      );
+    },
+
+    Footer() {
+      const { toSignIn } = useAuthenticator();
+      return (
+        <View className="text-center mt-4">
+          <p className="text-muted-foreground">
+            Already have an account?{" "}
+            <button
+              onClick={toSignIn}
+              className="text-primary hover:underline bg-transparent border-none p-0"
+            >
+              Sign in
+            </button>
+          </p>
+        </View>
+      );
+    },
+  },
+};
+
+export default Auth;
