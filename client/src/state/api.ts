47a57ec9243ef96602eadc3ee484f778da1fc59c
Kiet Dinh <kiet.dinh.t@gmail.com>
2026-01-19 19:42:02 -0600

    feat: 1st api route frontend 3:09:50

--- client/src/state/api.ts
+++ client/src/state/api.ts
@@ -1,12 +1,66 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { fetchAuthSession, getCurrentUser } from "aws-amplify/auth";
+import { Manager, Tenant } from "@/types/prismaTypes";
 
 export const api = createApi({
   baseQuery: fetchBaseQuery({
     baseUrl: process.env.NEXT_PUBLIC_API_BASE_URL,
+
+    // Attach Authorization header with all endpoint requests to make sure they are authenticated routes
+    prepareHeaders: async (headers) => {
+      const session = await fetchAuthSession();
+      const { idToken } = session.tokens ?? {};
+      if (idToken) {
+        headers.set("Authorization", `Bearer ${idToken}`);
+      }
+      return headers;
+    },
   }),
   reducerPath: "api",
   tagTypes: [],
-  endpoints: (build) => ({}),
+  endpoints: (build) => ({
+    getAuthUser: build.query<User, void>({
+      // fetchWithBQ: fetch with base query
+      queryFn: async (_, _queryApi, _extraoptions, fetchWithBQ) => {
+        try {
+          const session = await fetchAuthSession();
+          const user = await getCurrentUser();
+          const { idToken } = session.tokens ?? {};
+          const userRole = idToken?.payload["custom:role"] as string;
+
+          const endpoint =
+            userRole === "manager"
+              ? `/managers/${user.userId}`
+              : `/tenants/${user.userId}`;
+
+          let userDetailsResponse = await fetchWithBQ(endpoint);
+
+          // if user doesn't exist, create new user
+          // if (
+          //   userDetailsResponse.error &&
+          //   userDetailsResponse.error.status === 404
+          // ) {
+          //   userDetailsResponse = await createNewUserInDatabase(
+          //     user,
+          //     idToken,
+          //     userRole,
+          //     fetchWithBQ,
+          //   );
+          // }
+
+          return {
+            data: {
+              cognitoInfo: { ...user },
+              userInfo: userDetailsResponse.data as Tenant | Manager,
+              userRole,
+            },
+          };
+        } catch (error: any) {
+          return { error: error.message || "Could not fetch user data" };
+        }
+      },
+    }),
+  }),
 });
 
 export const {} = api;
