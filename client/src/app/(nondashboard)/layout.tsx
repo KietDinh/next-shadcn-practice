9aa9a242328f41590cbe838225393d7c7c0baeb2
KietDinh <kiet.dinh.t@gmail.com>
2026-01-20 19:30:52 -0600

    feat: @client first time trigger auth api routes

--- client/src/app/(nondashboard)/layout.tsx
+++ client/src/app/(nondashboard)/layout.tsx
@@ -1,8 +1,18 @@
+"use client";
+
 import Navbar from "@/components/Navbar";
 import { NAVBAR_HEIGHT } from "@/lib/constants";
-import React from "react";
+import { useGetAuthUserQuery } from "@/state/api";
+// import { usePathname, useRouter } from "next/navigation";
+// import React, { useEffect, useState } from "react";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
+  const { data: authUser, isLoading: authLoading } = useGetAuthUserQuery();
+  console.log("authUser:", authUser);
+  // const router = useRouter();
+  // const pathname = usePathname();
+  // const [isLoading, setIsLoading] = useState(true);
+
   return (
     <div className="h-full w-full">
       <Navbar />
